a5fe43b719116f8574329a1df3fee0627c68149c
Zackary Cammack <zack.cammack@gmail.com>
2020-08-29 16:11:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,4 +33,14 @@ class Song
     @@all.find{|song| song.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+    if @@all.find{|song| song.name == name}
+      song
+    else
+      song = self.new
+      song.name = name
+      @@all << song
+      song
+  end
+
 end
