ff7c912c97943589da73387bb1d633c603d3a54b
Zackary Cammack <zack.cammack@gmail.com>
2020-08-29 16:47:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
 
   def self.new_from_filename(filename)
     song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.name = (filename.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
     song
   end
