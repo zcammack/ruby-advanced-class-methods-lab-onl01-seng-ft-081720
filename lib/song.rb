bc62d3024f5c5b6b86af3858ce9a77d441d3ffd1
Zackary Cammack <zack.cammack@gmail.com>
2020-08-29 16:47:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,4 +41,10 @@ class Song
     @@all.sort_by{|x| x.name}
   end
 
+  def self.new_from_filename(filename)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
 end
